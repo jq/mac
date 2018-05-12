a865a0be856b003fc81db94ac9752c798c1986cf
尉来 <j.qian@antfin.com>
2018-05-12 15:03:23 +0800

    "update"

--- py.sh
+++ py.sh
@@ -1,6 +1,7 @@
 # for local error
 #export LC_ALL=en_US.UTF-8  
 #export LANG=en_US.UTF-8
+# if you have two versions of spark comment out SPARK_HOME
 #export SPARK_HOME=/usr/local/Cellar/apache-spark/2.3.0/libexec
 
 #http://docs.python-guide.org/en/latest/dev/virtualenvs/
